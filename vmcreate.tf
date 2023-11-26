8d5b07376be80ac0607c016797919e5e8b59b25c
ksanjay209 <66907976+ksanjay209@users.noreply.github.com>
2023-11-26 21:21:55 +0530

    Update vmcreate.tf

--- vmcreate.tf
+++ vmcreate.tf
@@ -1,6 +1,7 @@
 resource "azurerm_resource_group" "rg" {
 
-  name     = "${random_pet.prefix.id}-rg"
+
+  name     = "${var.prefix}-rg"
   location = var.resource_group_location
 }
 
@@ -8,7 +9,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "${random_pet.prefix.id}-vnet"
+  name                = "${var.prefix}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -16,7 +17,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "${random_pet.prefix.id}-subnet"
+  name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -24,7 +25,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "${random_pet.prefix.id}-public-ip"
+  name                = "${var.prefix}-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
@@ -72,6 +73,7 @@ resource "azurerm_network_interface" "my_terraform_nic" {
     name                          = "my_nic_configuration"
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
+    
 
   }
 }
@@ -107,7 +109,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   count                 = var.nodecount
 
   os_disk {
-    name                 = "myOsDisk"
+    name                 = "myOsDisk-${count.index}"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
