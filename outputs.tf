1ede8309a4fba5ca93411933683dafacd2507f67
ksanjay209 <66907976+ksanjay209@users.noreply.github.com>
2023-11-27 13:57:06 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,15 @@ output "admin_password" {
   sensitive = true
   value     = azurerm_windows_virtual_machine.main[*].admin_password
 }
+output "dcr_id" {
+  value = azurerm_monitor_data_collection_rule.dcrrule.id
+
+}
+output "dcr_name" {
+  value = azurerm_monitor_data_collection_rule.dcrrule.name
+
+}
+output "dcrassoc" {
+  value = azurerm_monitor_data_collection_rule_association.dcr_assoc.name
+
+}
